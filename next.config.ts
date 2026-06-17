33735e58e77eeeaedd59017fdb6ecc8c2c8a78be
Ilyas Alparslan Atmaca <ilyasalparslanatmaca@gmail.com>
2026-06-17 13:53:58 +0300

    fix: enforce static HTML export to bypass Vercel 404

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
   images: {
-    formats: ["image/avif", "image/webp"],
-    deviceSizes: [640, 750, 828, 1080, 1200],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256],
+    unoptimized: true,
   },
 };
 
